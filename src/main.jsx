9b6c3090db5007f7b8427eb699684e068dab3e66
maciu213 <janiejestemmacias@gmail.com>
2025-02-20 13:49:27 +0100

    animacje

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { useRef } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import AddTaskPage from './AddTaskPage';
-import RegisterLoginPage from './RegisterLoginPage';
-import LoginRegisterPage from './LoginRegisterPage';
-import Card from './components/Card';
-import './style.css';
+import React, { useRef, useState } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddTaskPage from "./AddTaskPage";
+import RegisterLoginPage from "./RegisterLoginPage";
+import LoginRegisterPage from "./LoginRegisterPage";
+import "./style.css";
 
 function App() {
-
   const dialogRef = useRef();
   const dialogRef2 = useRef();
   const dialogRef3 = useRef();
 
-  const addTaskDialog = () => {
-    dialogRef.current.showModal();
-  }
+  const [dialogState, setDialogState] = useState({
+    taskDialog: false,
+    registerDialog: false,
+    loginDialog: false
+  });
 
-  const registerShow = () => {
-    dialogRef2.current.showModal();
-  }
+  const openDialog = (dialogType) => {
+    setDialogState({ ...dialogState, [dialogType]: true });
+    dialogType === "taskDialog" && dialogRef.current.showModal();
+    dialogType === "registerDialog" && dialogRef2.current.showModal();
+    dialogType === "loginDialog" && dialogRef3.current.showModal();
+  };
 
-  const addTask = (event) => {
-    event.preventDefault();
-    console.log("klik");
-  }
-
-  const loginShow = () => {
-    dialogRef3.current.showModal();
-  }
-
+  const closeDialog = (dialogRef, dialogType) => {
+    dialogRef.current.close();
+    setDialogState({ ...dialogState, [dialogType]: false });
+  };
 
   return (
     <div className="flex">
       <div className="sidebar">
         <h2 className="sidebar-title">Menu</h2>
         <ul className="sidebar-menu">
-          <li className="sidebar-item" onClick={registerShow}>Register</li>
-          <li className="sidebar-item" onClick={loginShow}>Log in</li>
+          <li className="sidebar-item" onClick={() => openDialog("registerDialog")}>
+            Register
+          </li>
+          <li className="sidebar-item" onClick={() => openDialog("loginDialog")}>
+            Log in
+          </li>
           <li className="sidebar-item">Tasks</li>
         </ul>
       </div>
@@ -48,71 +49,124 @@ function App() {
           <h1>To-do-list</h1>
         </div>
 
-        <button onClick={addTaskDialog} className="add">Add task</button>
+        <button onClick={() => openDialog("taskDialog")} className="add">
+          Add task
+        </button>
 
-            <label htmlFor="">Add your task clicking the button above.</label>
-
-            <Card></Card>
-            <Card title="karta1" description="dwdw"></Card>
-            <Card title="karta2"></Card>
+        <label>Add your task by clicking the button above.</label>
       </div>
-      <dialog ref={dialogRef} className="dialogForm">
-      <form className="task-form">
-        <div className="form">
-          <strong id="formname">Add Task</strong> <br></br><br></br>
-          <label className="formLabel">Name <i>(Required)</i></label> <input type="text" name="name" /><br></br>
-          <label className="formLabel">Date <i>(Required)</i></label> <input type="date" name="date" /><br></br>
-          <label className="formLabel">Notes <i>(Optional)</i></label> <textarea name="notes" id="notes"></textarea><br></br>
-          <label className="formLabel">Category <i>(Optional)</i></label> <br></br>
-          <select name="category" id="category">
-            <option value="" disabled selected class="text-gray-500">Select a task</option>
-            <option>Category1</option>
-            <option>Category2</option>
-            <option>Category3</option>
-            <option>Category4</option>
-          </select> <br></br><br></br>
 
-          <button className="submitButton" onClick={addTask}>Submit</button>
-        </div>
-      </form>
+      {/* Add Task Dialog */}
+      <dialog ref={dialogRef} className={`dialogForm ${dialogState.taskDialog ? "show" : ""}`} id="dialogs">
+        <form className="task-form">
+          <div className="form">
+            <img
+              src="/closeicon.png"
+              className="closeIcon"
+              onClick={() => closeDialog(dialogRef, "taskDialog")}
+              alt="Close"
+            />
+            <strong id="formname">Add Task</strong> <br />
+            <br />
+            <label className="formLabel">Name (Required)</label>
+            <input type="text" name="name" />
+            <br />
+            <label className="formLabel">Date (Required)</label>
+            <input type="date" name="date" />
+            <br />
+            <label className="formLabel">Notes (Optional)</label>
+            <textarea name="notes" id="notes"></textarea>
+            <br />
+            <label className="formLabel">Category (Optional)</label> <br />
+            <select name="category" id="category">
+              <option value="" disabled selected>
+                Select a task
+              </option>
+              <option>Category1</option>
+              <option>Category2</option>
+              <option>Category3</option>
+              <option>Category4</option>
+            </select>
+            <br />
+            <br />
+            <button className="submitButton">Submit</button>
+          </div>
+        </form>
       </dialog>
 
-      <dialog ref={dialogRef2}>
-      <form className="task-form">
-        <div className="form">
-          <strong id="formname">Registration</strong> <br></br><br></br>
-          <label className="formLabel">Login</label> <input type="text" name="name" /><br></br><br></br>
-          <label className="formLabel">Password</label><input type="password" name="password" /><br></br> <br></br>
-          <label className="formLabel">Re-enter password</label><input type="password" name="password" /><br></br> <br></br>
-          <label className="formLabel">E-mail</label><input type="email" name="email" /><br></br> <br></br><br></br>
-          <button className="submitButton">Submit</button>
-        </div>
-      </form>
+      {/* Register Dialog */}
+      <dialog ref={dialogRef2} className={`dialogForm ${dialogState.registerDialog ? "show" : ""}`} id="dialogs">
+        <form className="task-form">
+          <div className="form">
+            <img
+              src="/closeicon.png"
+              className="closeIcon"
+              onClick={() => closeDialog(dialogRef2, "registerDialog")}
+              alt="Close"
+            />
+            <strong id="formname">Registration</strong> <br />
+            <br />
+            <label className="formLabel">Login</label>
+            <input type="text" name="name" />
+            <br />
+            <br />
+            <label className="formLabel">Password</label>
+            <input type="password" name="password" />
+            <br />
+            <br />
+            <label className="formLabel">Re-enter password</label>
+            <input type="password" name="password" />
+            <br />
+            <br />
+            <label className="formLabel">E-mail</label>
+            <input type="email" name="email" />
+            <br />
+            <br />
+            <br />
+            <button className="submitButton">Submit</button>
+          </div>
+        </form>
       </dialog>
 
-      <dialog ref={dialogRef3}>
-      <form className="task-form">
-        <div className="form">
-          <strong id="formname">Log in</strong> <br></br><br></br>
-          <label className="formLabel">Login</label> <input type="text" name="name" /><br></br><br></br>
-          <label className="formLabel">Password</label><input type="password" name="password" /><br></br> <br></br>
-          <label className="forgotPassword">Forgot password?</label><br></br><br></br>
-          <button className="submitButton">Submit</button>
-        </div>
-      </form>
+      {/* Login Dialog */}
+      <dialog ref={dialogRef3} className={`dialogForm ${dialogState.loginDialog ? "show" : ""}`} id="dialogs">
+        <form className="task-form">
+          <div className="form">
+            <img
+              src="/closeicon.png"
+              className="closeIcon"
+              onClick={() => closeDialog(dialogRef3, "loginDialog")}
+              alt="Close"
+            />
+            <strong id="formname">Log in</strong> <br />
+            <br />
+            <label className="formLabel">Login</label>
+            <input type="text" name="name" />
+            <br />
+            <br />
+            <label className="formLabel">Password</label>
+            <input type="password" name="password" />
+            <br />
+            <br />
+            <label className="forgotPassword">Forgot password?</label>
+            <br />
+            <br />
+            <button className="submitButton">Submit</button>
+          </div>
+        </form>
       </dialog>
     </div>
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/add-task" element={<AddTaskPage />} />
         <Route path="/register-login" element={<RegisterLoginPage />} />
-        <Route path="/login-register" element={<LoginRegisterPage/>} />
+        <Route path="/login-register" element={<LoginRegisterPage />} />
       </Routes>
     </Router>
   </React.StrictMode>
