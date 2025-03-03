29219cca68d47dade58de661211f818cff1abc5c
oklozet <kacperoco@o2.pl>
2025-03-03 09:16:37 +0100

    breeze and minor changes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,8 @@
 <?php
 
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\AuthController;
 
-Route::post('/register', [AuthController::class, 'register']); // For register
-Route::post('/login', [AuthController::class, 'login']); // For login
-
-
-Route::post('tasks', [TaskController::class, 'store']);  // For creating tasks
-Route::get('tasks', [TaskController::class, 'index']);   // For retrieving all tasks 
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
